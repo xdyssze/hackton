30df1f369d7fb234b0c7c6464161a5b2ca1c2009
Filip Kedland <filip.kedland@elev.ga.ntig.se>
2023-02-07 11:48:35 +0100

    tiff_get (download func)

---
+++ geotiff/tiff_get.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const request = require("request");
+
+
+function callback(message) {
+  console.warn(message);
+  return;
+}
+
+const download = (url, dest, cb) => {
+  const file = fs.createWriteStream(dest);
+  const sendReq = request.get(url);
+  
+  // Verify response code
+  sendReq.on("response", (response) => {
+      if (response.statusCode !== 200) {
+          return cb("Response status was " + response.statusCode);
+      }
+
+      sendReq.pipe(file);
+  });
+
+  file.on("finish", () => file.close(cb));
+
+  // Request errors
+  sendReq.on("error", (err) => {
+      fs.unlink(dest, () => cb(err.message)); // Delete the partial file
+  });
+
+  file.on("error", (err) => {
+      fs.unlink(dest, () => cb(err.message));
+  });
+};
+
+
+country = "SWE";
+height = "100";
+download(`https://globalwindatlas.info/api/gis/country/${country}/power-density/${height}`, "./db/test.tif", callback);
