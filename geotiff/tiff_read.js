204a4d6e06f17f0b5a109d8cdbe4ea55ee29007b
Filip Kedland <filip.kedland@elev.ga.ntig.se>
2023-02-07 15:16:41 +0100

    tiff_read

---
+++ geotiff/tiff_read.js
@@ -0,0 +1,17 @@
+import GeoTIFF, { fromUrl, fromUrls, fromArrayBuffer, fromBlob } from 'geotiff';
+import fetch from "node-fetch";
+
+async function get_power(country, height, coords) {
+  const response = await fetch("https://globalwindatlas.info/api/gis/country/SWE/power-density/100");
+  const arrayBuffer = await response.arrayBuffer();
+  const tiff = await fromArrayBuffer(arrayBuffer);
+
+  const data = await tiff.readRasters({
+    bbox: coords
+  });
+
+  console.log(data);
+}
+
+const coords = [16.474484, 56.559733, 16.502838, 56.543965];
+get_power("SWE", "100", coords);
